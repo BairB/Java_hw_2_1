05575b99b03235d23ae72eacdc054f5c2b4cbe7d
Bair B <bairu@yandex.ru>
2021-03-04 16:40:06 +0300

    report mod

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int cur_balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        long overall = cur_balance + transfer;
+        System.out.println("Общий баланс: " + overall);
+    }
 }
